d7c98d13c809a51a529188dcce6d99dd65e5c3ce
Jimmy Yin <118097335+xKranze@users.noreply.github.com>
2023-06-10 11:28:55 -0700

    added logo to header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,12 +11,14 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
+      <img src="/BuffinLogo.png" height="180px"></img>
         <div>
           <Link className="text-light" to="/">
             <h1 className="m-0">Sluffin or Buffin</h1>
           </Link>
           <p className="m-0">Stop Sluffin and get Buffin</p>
         </div>
+      
         <div>
           {Auth.loggedIn() ? (
             <>
